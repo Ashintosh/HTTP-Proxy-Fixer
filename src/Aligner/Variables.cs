069ba2dc92a0f7c034a4477d1d7b75af065752ee
Ashton Schmüidt <nicholas.ashton6425@gmail.com>
2019-11-28 17:22:49 -0500

    Add files via upload

---
+++ src/Aligner/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Aligner
+{
+    class Variables
+    {
+        public static string line;
+        public static string newLine;
+        public static string lineStarter = "http://";
+
+        public static string SAVE_PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ConvertedProxies.txt");
+        public static string filePath;
+
+        public static int lineCount = 0;
+    }
+}
